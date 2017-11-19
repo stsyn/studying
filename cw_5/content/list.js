28d33202f2d2ea1b845a83146fbf1332cbcbd1a6
stsyn <stasyan1902@gmail.com>
2017-11-19 17:13:55 +0500

    Add files via upload

---
+++ cw_5/content/list.js
@@ -0,0 +1,17 @@
+content.list = {
+c:`# Список литературы
+
+1. [The BitTorrent Protocol Specification](http://www.bittorrent.org/beps/bep_0003.html) [Электронный ресурс]. Дата обращения: [19.11.2017](http://web.archive.org/web/20171119111803/http://www.bittorrent.org/beps/bep_0003.html);
+7. [BitTorrent (протокол) — Википедия](https://ru.wikipedia.org/wiki/BitTorrent_(%D0%BF%D1%80%D0%BE%D1%82%D0%BE%D0%BA%D0%BE%D0%BB)) (в т.ч. [английская версия](https://en.wikipedia.org/wiki/BitTorrent)) [Электронный ресурс]. Дата обращения: [19.11.2017](https://web.archive.org/web/20171119103934/https://ru.wikipedia.org/wiki/BitTorrent_(%D0%BF%D1%80%D0%BE%D1%82%D0%BE%D0%BA%D0%BE%D0%BB));
+2. [BitTorrent-трекер — Википедия](https://ru.wikipedia.org/wiki/BitTorrent-%D1%82%D1%80%D0%B5%D0%BA%D0%B5%D1%80) [Электронный ресурс]. Дата обращения: [19.11.2017](https://web.archive.org/web/20171119104201/https://ru.wikipedia.org/wiki/BitTorrent-%D1%82%D1%80%D0%B5%D0%BA%D0%B5%D1%80));
+3. [BitTorrent. Как это работает и как с этим работать](https://xdrv.ru/articles/software/30/full) [Электронный ресурс]. Дата обращения: [19.11.2017](http://web.archive.org/web/20171119104429/https://xdrv.ru/articles/software/30/full);
+4. [Glossary of BitTorrent terms — Wikipedia](https://en.wikipedia.org/wiki/Glossary_of_BitTorrent_terms) [Электронный ресурс]. Дата обращения: [19.11.2017](http://web.archive.org/web/20171119104616/https://en.wikipedia.org/wiki/Glossary_of_BitTorrent_terms);
+5. [Как работает протокол BitTorrent](https://www.iguides.ru/main/other/kak_rabotaet_protokol_bittorrent/) [Электронный ресурс]. Дата обращения: [19.11.2017](http://web.archive.org/web/20171119104920/https://www.iguides.ru/main/other/kak_rabotaet_protokol_bittorrent/);
+6. [Поднимаем BitTorrent трекер — Журнал «Хакер» 04.09.2008](https://xakep.ru/2008/09/04/45007/) [Электронный ресурс]. Дата обращения: [19.11.2017](http://web.archive.org/web/20171119110759/https://xakep.ru/2008/09/04/45007/);
+7. [Torrent-файл. Что же у него внутри? / Хабрахабр](https://habrahabr.ru/post/119753/) [Электронный ресурс]. Дата обращения: [19.11.2017](http://web.archive.org/web/20171119115517/https://habrahabr.ru/post/119753/);
+`,
+markdown:true,
+prev:'components',
+name:'Список литературы',
+id:'list'
+};
