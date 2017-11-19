38e82f502f0fbc52e28f3860296d68560423550f
stsyn <stasyan1902@gmail.com>
2017-11-19 10:09:12 +0500

    Add files via upload

--- cw_5/content/part2.js
+++ cw_5/content/part2.js
@@ -1,7 +1,31 @@
 content.bittorrent = {
-c:``,
-markdown:true,
+multilevel:true,
 prev:'intro',
+first:'intro',
 name:'Протокол BitTorrent',
-id:'bittorrent'
-};
+id:'bittorrent',
+l2:{
+	intro:{
+		c:
+`#	Протокол BitTorrent
+
+##	Общие сведения
+
+![Лого BitTorrent](content/media/BitTorrent.png)
+
+`,
+		markdown:true,
+		name:'Общие сведения',
+		next:'working',
+		id:'intro'
+	},
+	
+	working:{
+		c:``,
+		markdown:true,
+		name:'Принцип работы',
+		prev:'intro',
+		id:'working'
+	}
+}
+}
